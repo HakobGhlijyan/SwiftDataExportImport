3ac303dcc9d5db173f5e4a25c770cf8ec75b2b63
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-12-17 19:24:20 +0400

    Code Clean

--- SwiftDataExportImport/ContentView.swift
+++ SwiftDataExportImport/ContentView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 import SwiftData
 import CryptoKit
 
-//MARK: - MODEL
 @Model
 class Transaction: Codable {
     var transactionName: String
@@ -48,19 +47,14 @@ class Transaction: Codable {
     }
 }
 
-//MARK: - Transaction Transferable
 struct TransactionTransferable: Transferable {
     var transactions: [Transaction]
     var key: String
     static var transferRepresentation: some TransferRepresentation {
         DataRepresentation(exportedContentType: .data) { dataTr in
-            //1: - encode
             let data = try JSONEncoder().encode(dataTr.transactions)
-            //2: - crypdo data for using key
             guard let encryptedData = try AES.GCM.seal(data, using: .key(dataTr.key)).combined else {
                 throw EncryptionError.encryptionFailed
-                //Комбинированный элемент, состоящий из одноразового номера, зашифрованных данных и тега аутентификации.
-                //Объединенное представление доступно только в том случае, если размер файла AES.GCM.Nonce по умолчанию равен 12 байтам. Структура данных комбинированного представления - это одноразовый номер, зашифрованный текст, затем тег.
             }
             return encryptedData
         }
@@ -71,7 +65,6 @@ struct TransactionTransferable: Transferable {
     }
 }
 
-//MARK: - KEY DATA FOR SHA256 -> string key convert for data sha256 key
 extension SymmetricKey {
     static func key(_ value: String) -> SymmetricKey {
         let keyData = value.data(using: .utf8)!
@@ -80,27 +73,23 @@ extension SymmetricKey {
     }
 }
 
-//MARK: - MODEL CATEGORY
 enum TransactionCategory: String, Codable {
     case income = "Income"
     case expense = "Expense"
 }
 
-//MARK: - VIEW
 struct ContentView: View {
-    //MARK: - QUERY
     @Query(sort: [.init(\Transaction.transactionDate, order: .reverse)], animation: .snappy) private var transactions: [Transaction]
     @Environment(\.modelContext) private var modelContext
-    
-    //MARK: - View Properties
+    //View Properties
     @State private var showAlertTF: Bool = false
     @State private var keyTF: String = ""
     
-    //MARK: - Exporter Properties
+    //Exporter Properties
     @State private var exportItem: TransactionTransferable?
     @State private var showFileExporter: Bool = false
     
-    //MARK: - Importer Properties
+    //Importer Properties
     @State private var showFileImporter: Bool = false
     @State private var importedURL: URL?
         
@@ -163,11 +152,11 @@ struct ContentView: View {
             }
         }
         .fileExporter(
-            isPresented: $showFileExporter,      // presenter
-            item: exportItem,                    // Item for transform
-            contentTypes: [.data],               // type -> data
-            defaultFilename: "Transactions",     // file name
-            onCompletion: { result in            // On export Actions...
+            isPresented: $showFileExporter,
+            item: exportItem,
+            contentTypes: [.data],
+            defaultFilename: "Transactions",
+            onCompletion: { result in
                 switch result {
                 case .success:
                     print("Success")
@@ -178,14 +167,14 @@ struct ContentView: View {
                 
                 exportItem = nil
             },
-            onCancellation: {                    // On Cancel and error
+            onCancellation: {
                 exportItem = nil
             }
         )
         .fileImporter(
-            isPresented: $showFileImporter,      // presenter
-            allowedContentTypes: [.data],        // type -> data
-            onCompletion: { result in            // On import Actions...
+            isPresented: $showFileImporter,
+            allowedContentTypes: [.data],
+            onCompletion: { result in
                 switch result {
                 case .success(let url):
                     importedURL = url
@@ -202,23 +191,16 @@ struct ContentView: View {
     private func exportData() {
         Task.detached(priority: .background) {
             do {
-                let container = try ModelContainer(for: Transaction.self) // Make Container
-                let context = ModelContext(container)                     // Make Context
-                //Не используйте контекст вашей локальной среды просмотра для извлечения объектов данных, это приведет к проблемам с производительностью. Вместо этого используйте отдельный контейнер модели для извлечения всех связанных объектов из модели данных.
-                
-//                let descriptor = FetchDescriptor(sortBy: <#T##[SortDescriptor<PersistentModel>]#>)
+                let container = try ModelContainer(for: Transaction.self)
+                let context = ModelContext(container)
                 let descriptor = FetchDescriptor(sortBy: [.init(\Transaction.transactionDate, order: .reverse)])
-                // Make Descriptor for exported , use model sort and reverrs order for saving
-                
                 let allObjects = try context.fetch(descriptor)
                 let exportItem = await TransactionTransferable(transactions: allObjects, key: keyTF)
-                                
                 await MainActor.run {
                     self.exportItem = exportItem
                     showFileExporter = true
                     keyTF = ""
                 }
-                
             } catch {
                 print(error.localizedDescription)
                 await MainActor.run {
@@ -232,34 +214,22 @@ struct ContentView: View {
         guard let url = importedURL else { return }
         Task(priority: .background) {
             do {
-                // START - получать доступ к ресурсу, защищенному с точки зрения безопасности
                 guard url.startAccessingSecurityScopedResource()  else { return }
+                let container = try ModelContainer(for: Transaction.self)
+                let context = ModelContext(container)
                 
-                let container = try ModelContainer(for: Transaction.self) // Make Container
-                let context = ModelContext(container)                     // Make Context
-                
-                // Data for use by URL -> POLUSHENIE PO URL DATA S CRYPTO ZAKADIROVANIM VIDE
                 let encryptedData = try Data(contentsOf: url)
-                // Data decrypte -> OTKRITIE ETOGO FILE PO KEY VVEDENOMU V TEXTFIELD I DECODIROVANIE
                 let decryptedData = try AES.GCM.open(.init(combined: encryptedData), using: .key(keyTF))
                 
-                // JSON DECODER -> decodirovanie poluchanix danix uje otkritix s pomochyu key , v data v vide model masiva
                 let allTransactions = try JSONDecoder().decode([Transaction].self, from: decryptedData)
-                
-                print(allTransactions.count)
-                
-                //For in V VEM MASIVE , INSERT V CONTEXT
+
                 for transaction in allTransactions {
                     context.insert(transaction)
                 }
-                
-                //SAVE
                 try context.save()
-                
                 keyTF = ""
                 
                 url.stopAccessingSecurityScopedResource()
-                // END - получать доступ к ресурсу, защищенному с точки зрения безопасности
             } catch {
                 print(error.localizedDescription)
                 await MainActor.run {
